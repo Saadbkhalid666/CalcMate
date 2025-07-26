5fb3c2979db48d633f90427a2d353bda39ec9e84
Saad bin khalid <khalidmehmoodeditz@gmail.com>
2025-07-26 13:31:48 +0500

    Update Hero.jsx

--- frontend/src/sections/Hero/Hero.jsx
+++ frontend/src/sections/Hero/Hero.jsx
@@ -6,14 +6,14 @@ export const Hero = () => {
   return (
     <div className="bg-[#3190ce] min-h-[100vh] w-full">
       <div
-        className="intro-container flex justify-center flex-col pb-20  pt-35"
+        className="intro-container flex justify-center flex-col pb-10  pt-35"
         id="home"
         data-aos="fade-up"
       >
         <h1 className="text-3xl text-white text-center font-extrabold font-sans">
           DaimTools - Calculator Suite
         </h1>
-        <p className="text-xl text-gray-100 text-center mt-3">
+        <p className="text-xl text-gray-100 text-center mt-1">
           All-in-One Online Calculators for Finance, Health, and Everyday Needs
           — Fast, Accurate, and Free.
         </p>
